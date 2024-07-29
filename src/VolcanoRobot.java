d4811d5d7ebf74965b95e064513361b168eee8c8
Aulejandro <aulejandromoreno@gmail.com>
2024-07-28 22:47:37 -0500

    Completed merge sort

---
+++ src/VolcanoRobot.java
@@ -0,0 +1,33 @@
+class VolcanoRobot {
+    String status;
+    int speed;
+    float temperature;
+    static int VolcanoRobotCount = 0;
+    VolcanoRobot(String status, int speed, float temperature){
+        this.status=status;
+        this.speed=speed;
+        this.temperature=temperature;
+        VolcanoRobotCount++;
+    }
+    VolcanoRobot(){
+        VolcanoRobotCount++;
+    }
+    void checkTemperature() {
+        if (temperature > 660) {
+            status = "returning home";
+            speed = 5;
+        }
+    }
+
+    void showAttributes() {
+        System.out.println("Status: " + status);
+        System.out.println("Speed: " + speed);
+        System.out.println("Temperature: " + temperature);
+    }
+    public static void main(String args[]){
+        VolcanoRobot one = new VolcanoRobot("Exploring", 15, 700 );
+        VolcanoRobot two =  new VolcanoRobot();
+        System.out.println("There are " + VolcanoRobot.VolcanoRobotCount + " VolcanoRobot Objects.");
+
+    }
+}
