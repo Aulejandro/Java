2ba25738b34ec394ef11018d94798dd4715451a5
Aulejandro <aulejandromoreno@gmail.com>
2024-08-08 17:05:01 -0500

    MaxSubArrayProblem

--- src/MaxSubArrayProblem.java
+++ src/MaxSubArrayProblem.java
@@ -6,19 +6,23 @@ public class MaxSubArrayProblem {
         int maxLeft = 0;
         int leftSum = Integer.MIN_VALUE;
         int sum = 0;
+
         for(int i = mid; i >= low; i--)
         {
             sum = sum + A[i];
-            if (sum > leftSum);
+            if(sum > leftSum)
             {
                 leftSum = sum;
                 maxLeft = i;
             }
         }
+
+
+        int maxRight = 0;
         int rightSum = Integer.MIN_VALUE;
         sum = 0;
-        int maxRight = 0;
-        for(int j = mid +1; j <= high; j++)
+
+        for(int j = mid + 1; j <= high; j++)
         {
             sum = sum + A[j];
             if(sum > rightSum)
@@ -27,16 +31,20 @@ public class MaxSubArrayProblem {
                 maxRight = j;
             }
         }
-        return new MaxSubArray(maxLeft,maxRight,leftSum +rightSum);
+
+        return new MaxSubArray(maxLeft,maxRight,leftSum + rightSum);
     }
+
     MaxSubArray findMaxSubArray(int[] A, int low, int high){
         if (high == low) {
          return new MaxSubArray(low, high, A[low]);
         }
         else{
+
             int mid = (low+high)/2;
+
             MaxSubArray left = findMaxSubArray(A, low, mid);
-            MaxSubArray right = findMaxSubArray(A,mid +1, high);
+            MaxSubArray right = findMaxSubArray(A,mid + 1, high);
             MaxSubArray crossing = findMaxCrossingSubarray(A, low, mid, high);
 
             if(left.sum >= right.sum && left.sum >= crossing.sum)
@@ -53,6 +61,6 @@ public class MaxSubArrayProblem {
         int[] change = {13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7};
         MaxSubArrayProblem m = new MaxSubArrayProblem();
         MaxSubArray maxSubArray = m.findMaxSubArray(change, 0, change.length-1);
-        System.out.println("Low: "+ maxSubArray.maxLeft+ " hight: "+ maxSubArray.maxRight+ " sum:"+ maxSubArray.sum);
+        System.out.println("Low: "+ maxSubArray.maxLeft+ " High: "+ maxSubArray.maxRight+ " Sum: "+ maxSubArray.sum);
     }
 }
